e4475ea30beaf18d2f10f21de271e80c608a03c4
leon.wei <leon.wei@gaiaworks.cn>
2020-04-24 16:53:45 +0800

    添加歌手界面，封装横向列表

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Horizen from '../../baseUI/horizen-item';
+import { categoryTypes, alphaTypes } from '../../api/config';
+import { NavContainer } from "./style";
 
-function Singers (props) {
+function Singers () {
+  let [category, setCategory] = useState ('');
+  let [alpha, setAlpha] = useState ('');
+
+  let handleUpdateAlpha = (val) => {
+    setAlpha (val);
+  }
+
+  let handleUpdateCatetory = (val) => {
+    setCategory (val);
+  }
 
   return (
-    <div>
-      <div>Singers</div>
-    </div>
+    <NavContainer>
+      <Horizen 
+        list={categoryTypes} 
+        title={"分类 (默认热门):"} 
+        handleClick={handleUpdateCatetory} 
+        oldVal={category}></Horizen>
+      <Horizen 
+        list={alphaTypes} 
+        title={"首字母:"} 
+        handleClick={val => handleUpdateAlpha (val)} 
+        oldVal={alpha}></Horizen>
+    </NavContainer>
   )
 }
 
