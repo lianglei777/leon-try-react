9afc45c9294c905fa97332f96434740fc03e5d9a
leon.wei <leon.wei@gaiaworks.cn>
2020-05-06 18:29:36 +0800

    添加歌手界面显示逻辑

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux-immutable';
 import { reducer as recommendReducer } from '../application/Recommend/store/index';
+import { reducer as singersReducer } from '../application/Singers/store/index';
+
 
 // 之后开发具体功能模块的时候添加 reducer
 export default combineReducers ({
 
     recommend: recommendReducer,
+    singers: singersReducer,
 
 });
