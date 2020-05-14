e46dbdbaeb0dd2c66e3c8a7a815ba07164d14b5f
leon.wei <leon.wei@gaiaworks.cn>
2020-05-14 17:47:31 +0800

    添加album界面内容，数据层

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,35 +1,48 @@
 // 数字转计数单位写法
 export const getCount = (count) => {
-    if (count < 0) return;
-    if (count < 10000) {
-        return count;
-    } else if (Math.floor(count / 10000) < 10000) {
-        return Math.floor(count / 1000) / 10 + "万";
-    } else {
-        return Math.floor(count / 10000000) / 10 + "亿";
-    }
+  if (count < 0) return;
+  if (count < 10000) {
+    return count;
+  } else if (Math.floor(count / 10000) < 10000) {
+    return Math.floor(count / 1000) / 10 + "万";
+  } else {
+    return Math.floor(count / 10000000) / 10 + "亿";
+  }
 }
 
 
 export const debounce = (func, delay) => {
-    let timer;
-    return function (...args) {
-      if(timer) {
-        clearTimeout(timer);
-      }
-      timer = setTimeout(() => {
-        func.apply(this, args);
-        clearTimeout(timer);
-      }, delay);
+  let timer;
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer);
     }
+    timer = setTimeout(() => {
+      func.apply(this, args);
+      clearTimeout(timer);
+    }, delay);
   }
+}
 
 
-  // 处理数据，找出第一个没有歌名的排行榜的索引
+// 处理数据，找出第一个没有歌名的排行榜的索引
 export const filterIndex = rankList => {
   for (let i = 0; i < rankList.length - 1; i++) {
-    if (rankList [i].tracks.length && !rankList [i + 1].tracks.length) {
+    if (rankList[i].tracks.length && !rankList[i + 1].tracks.length) {
       return i + 1;
     }
   }
 };
+
+// 处理歌手列表拼接歌手名字
+export const getName = list => {
+  let str = "";
+  list.map((item, index) => {
+    str += index === 0 ? item.name : "/" + item.name;
+    return item;
+  });
+  return str;
+};
+
+// 判断一个对象是否为空
+export const isEmptyObject = obj => !obj || Object.keys(obj).length === 0;
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux-immutable';
 import { reducer as recommendReducer } from '../application/Recommend/store/index';
 import { reducer as singersReducer } from '../application/Singers/store/index';
 import { reducer as rankReducer } from '../application/Rank/store/index';
+import { reducer as albumReducer } from '../application/Album/store/index';
 
 
 // 之后开发具体功能模块的时候添加 reducer
@@ -10,5 +11,6 @@ export default combineReducers ({
     recommend: recommendReducer,
     singers: singersReducer,
     rank: rankReducer,
+    album: albumReducer,
 
 });
